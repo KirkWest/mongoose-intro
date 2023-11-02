f49965cee5aba48fce4113f91c7089bbf41ad334
KirkWest <126360978+KirkWest@users.noreply.github.com>
2023-11-02 19:53:28 +1100

    feat: user schema and dotenv implemented

--- src/database.js
+++ src/database.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose');
 async function databaseConnect(){
 	try {
 		// DB connection can take some time, eg. if DB is in the cloud 
+		console.log("Connecting to:\n" + process.env.DB_URI);
 		await mongoose.connect(process.env.DB_URI);
 		console.log("Database connected");
 	} catch (error) {
